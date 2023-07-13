251369967c24d4ebb914ce87bb6148db604fe2f8
Fulvia Vaz <fulviavaz.dev@gmail.com>
2023-07-13 10:09:03 -0300

    add movies list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,30 @@
-import { FormsModule } from '@angular/forms';
+import { StarshipsComponent } from './components/pages/starships/starships.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FilmsComponent } from './components/pages/films/films.component';
-import { StarshipsComponent } from './components/pages/starships/starships.component';
-import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FilmsComponent,
-    StarshipsComponent,
-    HomeComponent
+    StarshipsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
     BrowserAnimationsModule,
+    FormsModule,
     MaterialModule,
-    FormsModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
